cb5bf58c5c259db6af89348d9ba3e553af323929
Ali Nuraldin <alirunwwc@gmail.com>
2023-03-03 15:53:34 +0100

    Added SourceId constants

---
+++ src/constants/SourceId.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+library SourceId {
+    uint256 constant BAB = uint256(keccak256("BAB"));
+    uint256 constant REALT = uint256(keccak256("REALT"));
+}
